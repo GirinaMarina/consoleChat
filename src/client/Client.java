87628998096054d5fbdcaf7a270ad08177229704
GirinaMarina <marina.girina1@gmail.com>
2023-03-17 15:34:24 +0300

    Add NicName at input of message

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -7,13 +8,37 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+
         try {
-            Socket socket = new Socket("127.0.0.1",9123);
+            Socket socket = new Socket("127.0.0.1", 9123);
             Scanner scanner = new Scanner(System.in);
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            String message;
+
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            System.out.println((in.readUTF()));
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Потеряно соединение с сервером");
+                    }
+                }
+            });
+
+            thread.start();
+
+            System.out.print("LogIn: ");
+            String nickName = scanner.nextLine();
+
             while (true) {
-                String message = scanner.nextLine();
-                out.writeUTF(message);
+                message = scanner.nextLine();
+                out.writeUTF(nickName + ": " + message);
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,19 +1,45 @@
 package server;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
     public static void main(String[] args) {
+        ArrayList<Socket> sockets = new ArrayList<>();
         try {
             ServerSocket serverSocket = new ServerSocket(9123);
             System.out.println("Сервер запущен");
-            Socket socket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(socket.getInputStream());
             while (true) {
-                System.out.println(in.readUTF());
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент подключился");
+                sockets.add(socket);
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            DataInputStream in = new DataInputStream(socket.getInputStream());
+                            String clientMessage;
+                            while (true) {
+                                clientMessage = in.readUTF();
+                                System.out.println(clientMessage);
+                                for (Socket socket : sockets) {
+                                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                                    out.writeUTF(clientMessage.toUpperCase());
+                                }
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Клиент отключился");
+                            sockets.remove(socket);
+                        }
+                    }
+                });
+
+                thread.start();
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
